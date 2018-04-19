ccdcbc3d16a68a686d20f2b30545726325d2cd77
ZooGorilla <38479795+ZooGorilla@users.noreply.github.com>
2018-04-19 11:05:33 -0500

    t
    t

--- exoplanetfinderv2.js
+++ exoplanetfinderv2.js
@@ -33,7 +33,7 @@
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_masse,pl_rade,rowupdate&order=dec&format=json", function(resp) {
                 
-				var resp2=JSON.parse(resp),
+				var resp2=JSON.parse(JSON.stringify(resp)),
 				tableData = [];
 			for (var i = 0, len = resp2.length; i < len; i++) {
                 tableData.push({
